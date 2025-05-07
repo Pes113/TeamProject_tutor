070c7ff923f607228f6d32a1e81842bb90c49888
Pes123 <dmstkd113@naver.com>
2025-05-07 11:22:25 +0900

    Fix: Rename ItcenStudController with corrected naming

--- src/main/java/com/example/teamproject/controller/ItcenStudContorller1.java
+++ src/main/java/com/example/teamproject/controller/ItcenStudContorller1.java
@@ -1,15 +1,16 @@
 package com.example.teamproject.controller;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 
 @Controller
-public class ItcenStudController {
+public class ItcenStudContorller1 {
 
-    @GetMapping("/itcenstud")
-    public @ResponseBody String itcenstud() {
-        return "Hello, Itcen Student!!";
+    @GetMapping("/itcenstud1")
+    public @ResponseBody String itcenstud1() {
+        return "Hello, itcenstud1!!";
     }
-
+    
 }
